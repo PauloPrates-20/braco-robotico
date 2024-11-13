31c5030286aab5ed22a749d5371211bf65884b83
Paulo Prates <paulo.prates01@hotmail.com>
2024-11-13 13:01:09 -0300

    refactor: axis organization

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,9 @@
 #include <Arduino.h>
-#include <PathPoints.hpp>
+#include <PathPoints.h>
 #include <VarSpeedServo.h>
 
 // Servo instanciation
-VarSpeedServo motorX;
-VarSpeedServo motorY;
-VarSpeedServo motorZ;
-VarSpeedServo motorA;
-
-// Initial motor angles
-int angleX = 120; 
-int angleY = 90; 
-int angleZ = 30;
-int angleA = 0;
+VarSpeedServo axis[4];
 
 // Base speed
 int speed = 75;
@@ -27,40 +18,18 @@ void setup() {
   while (!Serial) { };
 
   // Attach servos
-  motorX.attach(9);
-  motorY.attach(10);
-  motorZ.attach(11);
-  motorA.attach(12);
+  axis[ACTUATOR].attach(9);
+  axis[ROTATION].attach(10);
+  axis[ELEVATION].attach(11);
+  axis[EXTENSION].attach(12);
 
   // Zero the motors
-  motorX.write(angleX, speed, true);
-  motorY.write(angleY, speed, true);
-  motorZ.write(angleZ, speed, true);
-  motorA.write(angleA, speed, true);
+  axis[ACTUATOR].write(0, speed, true);
+  axis[ROTATION].write(90, speed, true);
+  axis[ELEVATION].write(30, speed, true);
+  axis[EXTENSION].write(120, speed, true);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int points;
-  std::vector<std::vector<int>> angles = setPoints(points);
-
-  for (int point = 0; point < points; point++) {
-    Serial.print("Point #");
-    Serial.print(point + 1);
-    Serial.print(" Angles: X=");
-    Serial.print(angles[point][ANGLE_X]);
-    Serial.print(", Y=");
-    Serial.print(angles[point][ANGLE_Y]);
-    Serial.print(", Z=");
-    Serial.print(angles[point][ANGLE_Z]);
-    Serial.print(", Claw=");
-    Serial.println(angles[point][ANGLE_A]);
-
-    motorX.write(angles[point][ANGLE_X], speed, true);
-    motorY.write(angles[point][ANGLE_Y], speed, true);
-    motorZ.write(angles[point][ANGLE_Z], speed, true);
-    motorA.write(angles[point][ANGLE_A], speed, true);
-  }
-
-  delay(2000);
 }
