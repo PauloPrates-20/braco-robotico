ebb1c95de77a2fba7fa8ea9202f089475fce224a
Paulo Prates <paulo.prates01@hotmail.com>
2024-11-13 13:00:49 -0300

    chore: path points class definition (wip)

---
+++ lib/PathPoints/PathPoints.cpp
@@ -0,0 +1,76 @@
+#include "PathPoints.h"
+
+// Serial functions
+// Waits for user input
+inline void waitInput() {
+    while (!(Serial.available() > 0)) { };
+}
+
+// Class declarations
+// Initializer
+PathPoints::PathPoints() {
+    _set = false;
+    _points = 0;
+}
+
+// Function to set the order of operation of the axis in a given point
+byte PathPoints::setOrder(uint8_t point, byte segment) {
+    Serial.println("(0) - Atuador, (1) - Rotação, (2) - Elevação, (3) - Extensão");
+    Serial.print("Eixo: ");
+    waitInput();
+    byte axis = Serial.parseInt();
+    _order[point][segment] = axis;
+    
+    switch (axis) {
+        case 0:
+            Serial.println("Atuador");
+            break;
+        case 1:
+            Serial.println("Rotação");
+            break;
+        case 2:
+            Serial.println("Evelevação");
+            break;
+        case 3:
+            Serial.println("Extensão");
+            break;
+    };
+
+    return axis;
+}
+
+// Function to set the angle of the axis in a given point
+void PathPoints::setAngle(uint8_t point, byte axis) {
+    Serial.print("Ângulo: ");
+    waitInput();
+    uint8_t angle = Serial.parseInt();
+    Serial.println(angle);
+
+    _path[point][axis] = angle;
+}
+
+void PathPoints::setPath() {
+    Serial.print("Points in path: ");
+    waitInput();
+    _points = Serial.parseInt();
+    Serial.println(_points);
+
+    for (uint8_t point = 0; point < _points; point++) {
+        Serial.print("Ponto #");
+        Serial.println(point);
+
+        for (byte segment = 0; segment < 4; segment++) {
+            byte axis = setOrder(point, segment);
+            setAngle(point, axis);
+        }
+    }
+
+    _set = true;
+}
+
+void PathPoints::editPoint() {
+    Serial.print("Point to edit: ");
+    waitInput();
+    uint8_t point = Serial.parseInt();
+    Serial.println(point);
+} 
---
+++ lib/PathPoints/PathPoints.h
@@ -0,0 +1,29 @@
+#ifndef __PATHPOINTS_H__
+#define __PATHPOINTS_H__
+
+#include "AxisReference.h"
+#include <Arduino.h>
+#include <ArduinoSTL.h>
+#include <vector>
+
+// Function declarations
+// Serial functions
+inline void waitInput(); // Function to wait for user input
+
+// Class declarations
+class PathPoints {
+    public:
+        PathPoints();
+        void setPath(); // Function to create a path
+        void editPoint(); // Function to edit a point within the path
+        void setAngle(uint8_t point, byte axis); // Function to set the axis' angles for a given point
+        byte setOrder(uint8_t point, byte segment); // Function to set the axis' order for a given point
+    
+    private:
+        std::vector<std::vector<int>> _path;
+        std::vector<std::vector<int>> _order;
+        uint8_t _points;
+        bool _set;
+};
+
+#endif
